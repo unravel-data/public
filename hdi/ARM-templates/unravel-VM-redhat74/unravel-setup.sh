1aa8bf29d00ebef33042bb541d42014973a88648
Raymond Lui <rlui@unraveldata.com>
2018-01-24 17:44:35 -0800

    updated unravel-VM-redhat74/azuredeploy.json and unravel-setup.sh script

--- hdi/ARM-templates/unravel-VM-redhat74/unravel-setup.sh
+++ hdi/ARM-templates/unravel-VM-redhat74/unravel-setup.sh
@@ -22,19 +22,17 @@ sleep 30
 mkdir -p /srv
 
 DATADISK=`/usr/bin/lsblk |grep 500G | awk '{print $1}'`
-
-#echo "/dev/${DATADISK}1  /srv  ext4 defaults 0 0" >> /etc/fstab
+echo $DATADISK > /tmp/datadisk
 echo "/dev/${DATADISK}1" > /tmp/dataprap
 
 echo "Partitioning Disk ${DATADISK}"
 echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/${DATADISK}
 
-
-
 DATAPRAP=`cat /tmp/dataprap`
+DDISK=`cat /tmp/datadisk`
 /usr/sbin/mkfs -t ext4 ${DATAPRAP}
 
-DISKUUID=`/usr/sbin/blkid |grep ext4 | awk '{ print $2}' |sed -e 's/"//g'`
+DISKUUID=`/usr/sbin/blkid |grep ext4 |grep $DDISK | awk '{ print $2}' |sed -e 's/"//g'`
 echo "${DISKUUID}    /srv   ext4 defaults  0 0" >> /etc/fstab
 
 /usr/bin/mount -a
