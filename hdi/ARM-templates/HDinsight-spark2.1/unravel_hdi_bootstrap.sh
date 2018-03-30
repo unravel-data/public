2ab201bbd01058df871e0ec6b65accb0a0252cd6
Unravel-Andy <36170790+Unravel-Andy@users.noreply.github.com>
2018-03-30 13:38:12 -0700

    Update unravel_hdi_bootstrap.sh
    [add] embedded a python script that runs separately and monitoring all ambari operations, once all operations complete will do a spark configuration check.
    
    [remove] if [ -f $AGENT_DST/$zip_name ]
    
    [add] -Dcom.unraveldata.client.rest.shutdown.ms=300

--- hdi/ARM-templates/HDinsight-spark2.1/unravel_hdi_bootstrap.sh
+++ hdi/ARM-templates/HDinsight-spark2.1/unravel_hdi_bootstrap.sh
@@ -666,7 +666,6 @@ function get_sensor_initd() {
 
 #set -x
 
-mkdir -p /tmp/unravel
 DAEMON_NAME="unravel_es"
 PID_FILE="${TMP_DIR}/\${DAEMON_NAME}.pid"
 OUT_LOG="${TMP_DIR}/\${DAEMON_NAME}.out"
@@ -1778,6 +1777,7 @@ function cluster_detect() {
     fi
   fi
   echo "HOST_ROLE=$HOST_ROLE" | tee -a ${OUT_FILE}
+  export HOST_ROLE=$HOST_ROLE
 }
 
 
@@ -1873,7 +1873,7 @@ function install_hh_aux_jars() {
 
   currentHiveEnvContent=$(bash $AMBARICONFIGS_SH -u $AMBARI_USR -p $AMBARI_PWD get $AMBARI_HOST $CLUSTER_ID hive-env 2>/dev/null | grep '"content"' | perl -lne 'print $1 if /"content" : "(.*)"/')
 
-  exportAuxJars="\nexport HIVE_AUX_JARS_PATH=\$HIVE_AUX_JARS_PATH:$jars_colon"
+  export AuxJars="\nexport HIVE_AUX_JARS_PATH=\$HIVE_AUX_JARS_PATH:$jars_colon"
   newHiveEnvContent="$currentHiveEnvContent$exportAuxJars"
 
   echo "Modifying hive-env" | tee -a ${OUT_FILE}
@@ -2216,6 +2216,60 @@ function spark_postinstall_check_impl() {
 
 }
 
+function final_check(){
+    echo "
+from subprocess import call, check_output
+import urllib2,base64,json,argparse
+from time import sleep
+log_dir='/tmp/unravel/'
+print('Removing Crontab job')
+call('( crontab -l | grep -v -F \'python %sfinal_check.py\' ) | crontab -' % log_dir ,shell=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('-host','--unravel-host', help='Unravel Server hostname', dest='unravel', required=True)
+parser.add_argument('-user','--username', help='ambari username', required=True)
+parser.add_argument('-pass','--password', help='ambari password', required=True)
+parser.add_argument('-c','--cluster_name', help='ambari cluster name', required=True)
+parser.add_argument('-s','--spark_ver', help='spark version', required=True)
+parser.add_argument('-l','--am_host', help='ambari host', required=True)
+argv = parser.parse_args()
+def am_req(api_name=None, full_api=None):
+    if api_name:
+        result = json.loads(check_output('curl -u {0}:\'{1}\' -s -H \'X-RequestedBy:ambari\' -X GET http://{2}:8080/api/v1/clusters/{3}/{4}'.format(argv.username, argv.password, argv.am_host, argv.cluster_name, api_name), shell=True))
+    elif full_api:
+        result = json.loads(check_output('curl -u {0}:\'{1}\' -s -H \'X-RequestedBy:ambari\' -X GET {2}'.format(argv.username, argv.password,full_api), shell=True))
+    return result
+def get_latest_req_stat():
+    cluster_requests = am_req(api_name='requests')
+    latest_cluster_req = cluster_requests['items'][-1]['href']
+    return (am_req(full_api=latest_cluster_req)['Requests']['request_status'])
+def get_spark_defaults():
+    try:
+        spark_defaults =check_output('/var/lib/ambari-server/resources/scripts/configs.py -l {0} -u {1} -p \'{2}\' -n {3} -a get -c spark-defaults'.format(argv.am_host, argv.username, argv.password, argv.cluster_name), shell=True)
+    except:
+        spark_defaults = check_output('/var/lib/ambari-server/resources/scripts/configs.py -l {0} -u {1} -p \'{2}\' -n {3} -a get -c spark2-defaults'.format(argv.am_host, argv.username, argv.password, argv.cluster_name), shell=True)
+    return (spark_defaults)
+def main():
+    sleep(30)
+    print('Checking Ambari Operations')
+    while(get_latest_req_stat() != 'COMPLETED'):
+        print('Operations Status:' + get_latest_req_stat())
+        sleep(30)
+    print('All Operations are completed, Comparing Spark config')
+    if get_spark_defaults().count('/var/log/spark') == 2:
+        print(get_spark_defaults() + '\n\nSpark Config is correct')
+    else:
+        print('Spark Config is not correct re-run unravel_hdi_bootstrap.sh')
+        call('wget https://raw.githubusercontent.com/unravel-data/public/master/hdi/ARM-templates/HDinsight-spark2.1/unravel_hdi_bootstrap.sh',shell=True)
+        call(['chmod', '+x', 'unravel_hdi_bootstrap.sh'])
+        call('./unravel_hdi_bootstrap.sh --unravel-server %s --spark-version %s' % (argv.unravel, argv.spark_ver),shell=True)
+if __name__ == '__main__':
+    main()
+
+
+" > /tmp/unravel/final_check.py
+    (crontab -l; echo "* * * * * sudo python /tmp/unravel/final_check.py -host ${UNRAVEL_SERVER} -l ${AMBARI_HOST} -user ${AMBARI_USR} -pass '${AMBARI_PWD}' -c ${CLUSTER_ID} -s ${SPARK_VER_XYZ} > /tmp/unravel/final_check.log") | crontab -
+}
+
 # dump the contents of env variables and shell settings
 debug_dump
 
@@ -2224,3 +2278,7 @@ allow_errors
 
 install -y $*
 
+# inject the python script
+if [ ${HOST_ROLE} == "master" ]; then
+    final_check
+fi
