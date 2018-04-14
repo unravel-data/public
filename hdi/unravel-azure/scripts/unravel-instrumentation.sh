89d1054c75affdd8658c8f90c18f9f69ad56ea33
Unravel-Andy <36170790+Unravel-Andy@users.noreply.github.com>
2018-04-13 17:23:00 -0700

    Update unravel-instrumentation.sh
    [remove] nohup command when uninstall unravel

--- hdi/unravel-azure/scripts/unravel-instrumentation.sh
+++ hdi/unravel-azure/scripts/unravel-instrumentation.sh
@@ -19,7 +19,7 @@ echo -e "\nCheck log files in /tmp/unravel/hdi_onpremises_setup.*\n"
 
 if [ $# -eq 1 ] && [ "$1" = "uninstall" ];then
    echo -e "\nUninstall Unravel\n"
-   nohup python /usr/local/unravel/hdi_onpremises_setup.py -uninstall> $TMP_DIR/hdi_onpremises_setup.log 2>$TMP_DIR/hdi_onpremises_setup.err &
+   python /usr/local/unravel/hdi_onpremises_setup.py -uninstall 2>&1
 else
    echo -e "\nInstall Unravel\n"
    nohup python /usr/local/unravel/hdi_onpremises_setup.py > $TMP_DIR/hdi_onpremises_setup.log 2>$TMP_DIR/hdi_onpremises_setup.err &
