547046bd02e0697f26e32c19de8142e285dd2c20
Unravel-Andy <36170790+Unravel-Andy@users.noreply.github.com>
2019-05-17 10:55:18 -0700

    HDI-28 Update ARN Template (#34)
    * HDI-28 Update ARN Template for 4.5.0.5
    
    * HDI-28 Update CentOS 73 ARN Template

--- hdi/ARM-templates/unravel-VM-redhat74x/unravel-setup.sh
+++ hdi/ARM-templates/unravel-VM-redhat74x/unravel-setup.sh
@@ -1,5 +1,46 @@
 # Download unravel rpm
-/usr/bin/wget http://preview.unraveldata.com/unravel/RPM/4.2.7/Azure/unravel-4.2.7-Azure-latest.rpm
+ver_comp () {
+    # $1 == $2 return 0
+    # $1 < $2 return 2
+    # $1 > $2 return 1
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+RPM_URL="https://preview.unraveldata.com/img/unravel-4.2.7-Azure-latest.rpm"
+
+if [ $# -eq 9 ]; then
+ RPM_URL=$9
+fi
+RPM_VER=$(echo ${RPM_URL} | grep -m 1 -o '[4-5].[0-9].[0-9]' | grep -m 1 '[4-5].[0-9].[0-9]')
+
+/usr/bin/wget ${RPM_URL}
 
 BLOBSTORACCT=${1}
 BLOBPRIACKEY=${2}
@@ -49,29 +90,52 @@ echo "${DISKUUID}    /srv   ext4 defaults  0 0" >> /etc/fstab
 /usr/bin/mount -a
 
 # install unravel rpm
-/usr/bin/rpm  -U unravel-4.2.7-Azure-latest.rpm
+/usr/bin/rpm  -U $(basename $RPM_URL)
 
 /usr/bin/sleep 5
 
 
 # Update Unravel Lic Key into the unravel.properties file
 # Obtain a valid unravel Lic Key file ; the following is just non working one
-echo "com.unraveldata.lic=1p6ed4s492012j5rb242rq3x3w702z1l455g501z2z4o2o4lo675555u3h" >> /usr/local/unravel/etc/unravel.properties
-echo "export CDH_CPATH="/usr/local/unravel/dlib/hdp2.6.x/*"" >> /usr/local/unravel/etc/unravel.ext.sh
+UN_PROP_PATH='/usr/local/unravel/etc/unravel.properties'
+echo "com.unraveldata.lic=1p6ed4s492012j5rb242rq3x3w702z1l455g501z2z4o2o4lo675555u3h" >> ${UN_PROP_PATH}
+
+# Compare Unravel Version add CDH_CPATH if older than 4.5
+ver_comp $RPM_VER 4.5.0
+if [[ $? -eq 2 ]]; then
+    echo "export CDH_CPATH='/usr/local/unravel/dlib/hdp2.6.x/*'" >> /usr/local/unravel/etc/unravel.ext.sh
+fi
+
+# run switch to user for version 4.3 and higher
+ver_comp $RPM_VER 4.2.7
+if [[ $? -eq 1 ]]; then
+    sudo useradd  hdfs
+    sudo groupadd hadoop
+    sudo usermod -a -G hadoop hdfs
+    sudo /usr/local/unravel/install_bin/switch_to_user.sh hdfs hadoop
+fi
+
 
 # Update Azure blob storage account credential in unravel.properties file
 # Update and uncomment the following lines to reflect your Azure blob storage account name and keys
 
-if [ $BLOBSTORACCT != "NONE" ] && [ $BLOBPRIACKEY != "NONE" ] && [ $BLOBSECACKEY != "NONE" ]; then
+if [ $BLOBSTORACCT != "NONE" ] && [ $BLOBPRIACKEY != "NONE" ]; then
 
    echo "blob storage account name is ${BLOBSTORACCT}"
    echo "blob primary access key is ${BLOBPRIACKEY}"
    echo "blob secondary access key is ${BLOBSECACKEY}"
-   echo "# Adding Blob Storage Account information, Update and uncomment following lines" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.hdinsight.storage-account-name-1=fs.azure.account.key.${BLOBSTORACCT}.blob.core.windows.net" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.hdinsight.primary-access-key=${BLOBPRIACKEY}" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.hdinsight.storage-account-name-2=fs.azure.account.key.${BLOBSTORACCT}.blob.core.windows.net" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.hdinsight.secondary-access-key=${BLOBSECACKEY}" >> /usr/local/unravel/etc/unravel.properties
+   echo "# Adding Blob Storage Account information, Update and uncomment following lines" >> ${UN_PROP_PATH}
+   # Blob storage properties for Unravel 4.5.x and newer
+   ver_comp $RPM_VER 4.4.3
+   if [[ $? -eq 1 ]]; then
+       echo "com.unraveldata.hdinsight.storage-account.1=fs.azure.account.key.${BLOBSTORACCT}.blob.core.windows.net" >> ${UN_PROP_PATH}
+       echo "com.unraveldata.hdinsight.access-key.1=${BLOBPRIACKEY}" >> ${UN_PROP_PATH}
+   else
+       echo "com.unraveldata.hdinsight.storage-account-name-1=fs.azure.account.key.${BLOBSTORACCT}.blob.core.windows.net" >> ${UN_PROP_PATH}
+       echo "com.unraveldata.hdinsight.primary-access-key=${BLOBPRIACKEY}" >> ${UN_PROP_PATH}
+       echo "com.unraveldata.hdinsight.storage-account-name-2=fs.azure.account.key.${BLOBSTORACCT}.blob.core.windows.net" >> ${UN_PROP_PATH}
+       echo "com.unraveldata.hdinsight.secondary-access-key=${BLOBSECACKEY}" >> ${UN_PROP_PATH}
+   fi
 
 else
    echo "One or more of your blob storage account parameter is invalid, please check your parameter file"
@@ -86,12 +150,12 @@ if [ $DLKSTOREACCT != "NONE" ] && [ $DLKCLIENTAID != "NONE" ] && [ $DLKCLIENTKEY
    echo "Data Lake Client Key is ${DLKCLIENTKEY}"
    echo "Data Lake Access Token is ${DLKCLITOKEPT}"
    echo "Data Lake Client Root Path is ${DLKCLIROPATH}"
-   echo "# Adding Data Lake Account information, Update and uncomment following lines" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.adl.accountFQDN=${DLKSTOREACCT}.azuredatalakestore.net" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.adl.clientId=${DLKCLIENTAID}" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.adl.clientKey=${DLKCLIENTKEY}" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.adl.accessTokenEndpoint=${DLKCLITOKEPT}" >> /usr/local/unravel/etc/unravel.properties
-   echo "com.unraveldata.adl.clientRootPath=${DLKCLIROPATH}" >> /usr/local/unravel/etc/unravel.properties
+   echo "# Adding Data Lake Account information, Update and uncomment following lines" >> ${UN_PROP_PATH}
+   echo "com.unraveldata.adl.accountFQDN=${DLKSTOREACCT}.azuredatalakestore.net" >> ${UN_PROP_PATH}
+   echo "com.unraveldata.adl.clientId=${DLKCLIENTAID}" >> ${UN_PROP_PATH}
+   echo "com.unraveldata.adl.clientKey=${DLKCLIENTKEY}" >> ${UN_PROP_PATH}
+   echo "com.unraveldata.adl.accessTokenEndpoint=${DLKCLITOKEPT}" >> ${UN_PROP_PATH}
+   echo "com.unraveldata.adl.clientRootPath=${DLKCLIROPATH}" >> ${UN_PROP_PATH}
 
 else
   echo "One or more of your data lake storge parameter is invalid, please check your parameter file"
@@ -99,10 +163,10 @@ fi
 
 # Adding unravel properties for Azure Cloud
 
-echo "com.unraveldata.onprem=false" >> /usr/local/unravel/etc/unravel.properties
-echo "com.unraveldata.spark.live.pipeline.enabled=true" >> /usr/local/unravel/etc/unravel.properties
-echo "com.unraveldata.spark.appLoading.maxAttempts=10" >> /usr/local/unravel/etc/unravel.properties
-echo "com.unraveldata.spark.appLoading.delayForRetry=4000" >> /usr/local/unravel/etc/unravel.properties
+echo "com.unraveldata.onprem=false" >> ${UN_PROP_PATH}
+echo "com.unraveldata.spark.live.pipeline.enabled=true" >> ${UN_PROP_PATH}
+echo "com.unraveldata.spark.appLoading.maxAttempts=10" >> ${UN_PROP_PATH}
+echo "com.unraveldata.spark.appLoading.delayForRetry=4000" >> ${UN_PROP_PATH}
 
 # Starting Unravel daemons
 # uncomment below will start unravel daemon automatically but within unravel_all.sh start  will have exit status=1.
