baabd35f4f4608b429ab81a41d4651232b22038b
Vivek Verma <vverma@unraveldata.com>
2020-12-16 17:24:16 +0530

    added support for mr,tez,hive

--- hdi/hdinsight-unravel-spark-script-action/unravel_hdi_spark_bootstrap_4.5.sh
+++ hdi/hdinsight-unravel-spark-script-action/unravel_hdi_spark_bootstrap_4.5.sh
@@ -3383,6 +3383,7 @@ hive_site_configs = {'hive.exec.driver.run.hooks': ['com.unraveldata.dataflow.hi
                     'com.unraveldata.hive.hdfs.dir': ['/user/unravel/HOOK_RESULT_DIR'],
                     'com.unraveldata.hive.hook.tcp': ['true'],
                     'com.unraveldata.host': [argv.unravel],
+                    'com.unraveldata.port': [argv.lr_port],
                     'hive.exec.pre.hooks': ['com.unraveldata.dataflow.hive.hook.{0}', 'HivePreHook'],
                     'hive.exec.post.hooks': ['com.unraveldata.dataflow.hive.hook.{0}', 'HivePostHook'],
                     'hive.exec.failure.hooks': ['com.unraveldata.dataflow.hive.hook.{0}', 'HiveFailHook'],
@@ -3431,14 +3432,14 @@ if hdfs_url.startswith('adl'):
 
 mapred_site_configs = None
 if argv.all:
-    mapred_site_configs = {'yarn.app.mapreduce.am.command-opts': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={4}', agent_path, argv.unravel_lr, argv.lr_port, argv.metrics_factor, "false"],
+    mapred_site_configs = {'yarn.app.mapreduce.am.command-opts': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={4}', agent_path, argv.unravel, argv.lr_port, argv.metrics_factor, "false"],
                         'mapreduce.task.profile': ['true'],
                         'mapreduce.task.profile.maps': ['0-5'],
                         'mapreduce.task.profile.reduces': ['0-5'],
-                        'mapreduce.task.profile.params': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr{4} -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={5}', agent_path, argv.unravel_lr, argv.lr_port, argv.metrics_factor, ",clusterId=" + argv.cluster_name, "false"]}
+                        'mapreduce.task.profile.params': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr{4} -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={5}', agent_path, argv.unravel, argv.lr_port, argv.metrics_factor, ",clusterId=" + argv.cluster_name, "false"]}
 tez_site_configs = {
-                    'tez.am.launch.cmd-opts': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr,config=tez{4} -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={5}', agent_path, argv.unravel_lr, argv.lr_port, argv.metrics_factor, ",clusterId=" + argv.cluster_name, "false"],
-                    'tez.task.launch.cmd-opts': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr,config=tez{4} -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={5}', agent_path, argv.unravel_lr, argv.lr_port, argv.metrics_factor, ",clusterId=" + argv.cluster_name, "false"]
+                    'tez.am.launch.cmd-opts': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr,config=tez{4} -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={5}', agent_path, argv.unravel, argv.lr_port, argv.metrics_factor, ",clusterId=" + argv.cluster_name, "false"],
+                    'tez.task.launch.cmd-opts': ['-javaagent:{0}/jars/btrace-agent.jar=libs=mr,config=tez{4} -Dunravel.server.hostport={1}:{2} -Dunravel.metrics.factor={3} -Dcom.unraveldata.client.resolve.hostname={5}', agent_path, argv.unravel, argv.lr_port, argv.metrics_factor, ",clusterId=" + argv.cluster_name, "false"]
                     }
 if argv.esp and argv.principal:
     tez_site_configs['tez.am.view-acls'] = [argv.principal]
