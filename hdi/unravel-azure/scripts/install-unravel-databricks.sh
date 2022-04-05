60aa330047a02194059f5ddb6518784c9f9113af
Unravel-Andy <36170790+Unravel-Andy@users.noreply.github.com>
2022-04-05 14:41:08 -0700

    Update install-unravel-databricks.sh

--- hdi/unravel-azure/scripts/install-unravel-databricks.sh
+++ hdi/unravel-azure/scripts/install-unravel-databricks.sh
@@ -3,9 +3,18 @@ MYSQL_USER=$2
 MYSQL_PASS=$3
 
 PACKAGE_LOC="/unravel"
+UN_ROOT_PATH="/opt/unravel"
+UNRAVEL_USER="unravel"
 
-if [ ! -d $PACKAGE_LOC ]; then
-    mkdir -p $PACKAGE_LOC
+if [ ! -d $UN_ROOT_PATH ]; then
+    mkdir -p $UN_ROOT_PATH
+fi
+
+# check user exists
+id -u $UNRAVEL_USER > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "Creating user $UNRAVEL_USER"
+    useradd -m "$UNRAVEL_USER"
 fi
 
 # Prepare the VM for unravel rpm install
@@ -21,10 +30,9 @@ fi
 
 #sleep 30
 
-grep srv /etc/fstab
+grep ${UN_ROOT_PATH} /etc/fstab
 # Prepare disk for unravel
 if [[ $? -eq 1 ]] && [[ -e "/dev/sdc" ]]; then
-    mkdir -p /srv
 
     DATADISK=`/usr/bin/lsblk |grep sdc | awk '{print $1}'`
     echo $DATADISK > /tmp/datadisk
@@ -38,55 +46,20 @@ if [[ $? -eq 1 ]] && [[ -e "/dev/sdc" ]]; then
     /usr/sbin/mkfs -t ext4 ${DATAPRAP}
 
     DISKUUID=`/usr/sbin/blkid |grep ext4 |grep $DDISK  | awk '{ print $2}' |sed -e 's/"//g'`
-    echo "${DISKUUID}    /srv   ext4 defaults  0 0" >> /etc/fstab
+    echo "${DISKUUID}    ${UN_ROOT_PATH}   ext4 defaults  0 0" >> /etc/fstab
 
     /usr/bin/mount -a
-    mkdir -p /srv/local/unravel
-    ln -s /srv/local/unravel /usr/local/unravel
 fi
 
-# install unravel rpm
-/usr/bin/rpm  -U $PACKAGE_LOC/unravel-*.rpm
+# install unravel tarball
+tar -xf ${PACKAGE_LOC}/unravel-*.tar.gz -C "$(dirname $UN_ROOT_PATH)"
+chown -R "$UNRAVEL_USER":"$UNRAVEL_USER" ${UN_ROOT_PATH}
 
-/usr/bin/sleep 5
-
-sudo chkconfig unravel_pg off
-sudo chkconfig unravel_cw off
-
-UN_ROOT_PATH="/usr/local/unravel"
-cd $PACKAGE_LOC
-tar xvzf $PACKAGE_LOC/mysql-connector-java-5.1.47.tar.gz
-JDBC_JAR="$PACKAGE_LOC/mysql-connector-java-5.1.47/mysql-connector-java-5.1.47.jar"
-JDBC_URL="jdbc:mariadb://${MYSQL_HOST}:3306/unravel_mysql_prod"
-if [ -f $JDBC_JAR ];then
-    sudo mkdir -p $UN_ROOT_PATH/share/java
-    sudo cp $JDBC_JAR $UN_ROOT_PATH/share/java/
-     JDBC_URL="jdbc:mysql://${MYSQL_HOST}:3306/unravel_mysql_prod"
-fi
-
-sudo -u unravel python $UN_ROOT_PATH/install_bin/properties_tracker.py
-
-# Update Unravel Lic Key into the unravel.properties file
-# Obtain a valid unravel Lic Key file ; the following is just a fake one
-UN_PROP_PATH='/usr/local/unravel/etc/unravel.properties'
-echo "com.unraveldata.lic=1p6ed4s492012j5rb242rq3x3w702z1l455g501z2z4o2o4lo675555u3h" >> ${UN_PROP_PATH}
-
-# Adding unravel properties for Azure Databricks Cloud
-echo "com.unraveldata.onprem=false" >> ${UN_PROP_PATH}
-echo "com.unraveldata.cluster.type=DB" >> ${UN_PROP_PATH}
-echo "com.unraveldata.python.enabled=false" >> ${UN_PROP_PATH}
-echo "com.unraveldata.tagging.enabled=true" >> ${UN_PROP_PATH}
+sudo -u "$UNRAVEL_USER" ${UN_ROOT_PATH}/versions/*/setup --extra ${PACKAGE_LOC}  --external-database mysql "$MYSQL_HOST" 3306 "unravel_mysql_prod" "$MYSQL_USER" "$MYSQL_PASS" --enable-databricks --skip-precheck
 
 
-cat <<EOF >> ${UN_PROP_PATH}
-unravel.jdbc.password=${MYSQL_PASS}
-unravel.jdbc.url=${JDBC_URL}
-unravel.jdbc.username=${MYSQL_USER}
-EOF
 
-/usr/local/unravel/dbin/db_schema_upgrade.sh
-/usr/local/unravel/install_bin/db_initial_inserts.sh | /usr/local/unravel/install_bin/db_access.sh
 
 # Starting Unravel daemons
-/etc/init.d/unravel_all.sh start
+sudo -u "$UNRAVEL_USER" ${UN_ROOT_PATH}/manager start
 exit 0
